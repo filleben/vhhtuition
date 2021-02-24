32cfb5a76cc11181058386627c3e135dfd96e600
filleben <filleryben@gmail.com>
2021-02-24 14:43:16 +0000

    added cookie and privacy policy pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy-policy/', views.privacy, name='privacy'),
+    path('cookie-policy/', views.cookie, name='cookie'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def home(request):
- return render(request, 'home/index.html')
+    return render(request, 'home/index.html')
+
+
+def privacy(request):
+    return render(request, 'home/privacy_policy.html')
+
+
+def cookie(request):
+    return render(request, 'home/cookie_policy.html')
