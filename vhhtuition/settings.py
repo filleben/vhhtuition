7d8673e6c22ab1123ce125f049a459ac9d77d947
filleben <filleryben@gmail.com>
2021-02-25 13:28:41 +0000

    imported heroku settings

--- vhhtuition/settings.py
+++ vhhtuition/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,3 +165,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
