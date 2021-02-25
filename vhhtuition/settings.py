8650cd101975595e11837c0d5123b3314febfb9e
filleben <filleryben@gmail.com>
2021-02-25 12:45:58 +0000

    fixed typo

--- vhhtuition/settings.py
+++ vhhtuition/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
