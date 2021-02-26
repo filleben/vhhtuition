796b3bb8c585f6632b23af55c9e23a3b87262d26
filleben <filleryben@gmail.com>
2021-02-26 10:51:55 +0000

    renamed weekly challenges to challenges

--- weekly_challenges/views.py
+++ weekly_challenges/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def display_challenges(request):
-    return render(request, 'weekly_challenges/weekly_challenges.html')
+    return render(request, 'weekly_challenges/challenges.html')
